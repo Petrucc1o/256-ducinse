96e4f6fda5eede66435c655b670cd7f340d080d4
P. Luz <100733955+Petrucc1o@users.noreply.github.com>
2022-03-08 20:48:24 -0300

    added bot.py
    will handle all of the discord-related processes, communicating with the module rolldice.py

---
+++ bot.py
@@ -0,0 +1,29 @@
+
+import diceroll
+import discord
+from discord.ext import commands
+import asyncio
+
+dice = ''
+client = commands.Bot(command_prefix='!')
+
+@client.event
+async def on_ready():
+    print('\n!!!= = = 256 Status: On = = =!!!\n')
+
+
+@client.event
+async def on_message(message):
+    channel = message.channel
+    msg = message.content
+    print(message)
+
+    if 'd' in message.content and message.content[0].isdecimal:
+        dice = diceroll.roll(msg)
+
+        await channel.send(dice)    
+        
+        
+
+from tokenbot import token256
+client.run(token256())
